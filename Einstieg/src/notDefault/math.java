169aef43e19a27c8b321a442f73739ac430ac747
Pascal Thomas <pascal.thomas@it-bc.de>
2017-09-12 22:51:03 +0200

    added method to math

--- Einstieg/src/notDefault/math.java
+++ Einstieg/src/notDefault/math.java
@@ -3,11 +3,17 @@ package notDefault;
 public class math {
 
 	public static void main(String[] args) {
-		int i = 0;
+		int i = 1;
 		int b = 5;
-		i = 1 + 2;
-		System.out.println(i+b);
+		System.out.println(calc(i, b));
 
 	}
 
+
+
+	public static int calc(int a, int b) {
+		int erg = 0;
+		erg = (a + 1) * (b + 1);
+		return erg;
+	}
 }
