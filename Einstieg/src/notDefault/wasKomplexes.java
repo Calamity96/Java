4f5f5e1878cad6da26fa428d65eee2d9cc9a3c2e
Pascal Thomas <pascal.thomas@it-bc.de>
2017-09-14 23:50:59 +0200

    an wasKomplexes rumgespielt

--- Einstieg/src/notDefault/wasKomplexes.java
+++ Einstieg/src/notDefault/wasKomplexes.java
@@ -1,9 +1,25 @@
 package notDefault;
 
+import java.util.*;
+
 public class wasKomplexes {
 
+	public String something;
+	
 	public static void main(String[] args) {
-		
+		wasKomplexes x = new wasKomplexes();
+		x.ausf();
 	}
-
+	
+	public void ausf() {
+		System.out.println("Geben sie was ein: ");
+		lesen();
+		System.out.println(this.something);
+	}
+	
+	public void lesen () {
+		Scanner scan = new Scanner(System.in);
+		this.something = scan.next();
+		scan.close();
+	}
 }
