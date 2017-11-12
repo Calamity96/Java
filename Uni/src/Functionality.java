06085baded9c6f18d3a7bef7b2fd45b149996c23
Pascal Thomas <pascal.thomas@it-bc.de>
2017-11-12 19:33:31 +0100

    HA done

--- Uni/src/Functionality.java
+++ Uni/src/Functionality.java
@@ -1,15 +1,23 @@
-
 public class Functionality {
-	
-	public static int quersumme(int n) {
-		int num = n;
-		if (n<0) n = n*(-1);
-		int qs=0;
-		while(n>0) {
-			qs = qs + (n % 10);
-			n = n/10;
+
+	public static int[][] insertionSort(int[] a) {
+		if(a==null) {
+			return null;
+		}else {
+			int[][] sorted = new int[a.length][a.length];
+			sorted[0]=a.clone();
+			for(int i=1;i<a.length;i++) {
+				sorted[i] = sorted[i-1].clone();
+				for(int j=0;j<i;j++) {
+					if(sorted[i][i]<sorted[i][j]) {
+						int swap = sorted[i][i];
+						sorted[i][i] = sorted[i][j];
+						sorted[i][j] = swap;
+					}
+				}
+			}
+			return sorted;
 		}
-		if (num<0) qs = qs*(-1);
-		return qs;
 	}
+	
 }
